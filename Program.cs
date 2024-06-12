2f485cc68aa3c01ac12bf2a491b62b08bf162fff
User <User@W11PC>
2024-06-12 10:45:03 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Основной поток начал работу.");
+
+        Parallel.Invoke(Method1, Method2);
+        //Method1();
+        //Method2();
+        Console.WriteLine("Основной поток завершил работу.");
+        Console.ReadLine();
+    }
+
+    static void Method1()
+    {
+        Console.WriteLine("Метод 1 начал работу.");
+        //Thread.Sleep(2000);
+        Console.WriteLine("Метод 1 завершил работу.");
+    }
+
+    static void Method2()
+    {
+        Console.WriteLine("Метод 2 начал работу.");
+        //Thread.Sleep(2000);
+        Console.WriteLine("Метод 2 завершил работу.");
+    }
+
+    static void Method3()
+    {
+        Console.WriteLine("Метод 3 начал работу.");
+        Thread.Sleep(2000);
+        Console.WriteLine("Метод 3 завершил работу.");
+    }
+    }
